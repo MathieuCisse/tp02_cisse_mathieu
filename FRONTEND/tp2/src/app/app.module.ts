c49876a7ffa0223de0577a70ab4e0b799feb0340
Mathieu C <mathieu.cisse@cirkwi.com>
2021-11-29 23:00:39 +0100

    Ajout composant + Amélioration design

--- FRONTEND/tp2/src/app/app.module.ts
+++ FRONTEND/tp2/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+import { RecapComponent } from './recap/recap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TetiereComponent,
+    FooterComponent,
+    FormComponent,
+    RecapComponent
   ],
   imports: [
     BrowserModule
